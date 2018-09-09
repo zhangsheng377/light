bb18b29ae83ef48c4ee8b61a203846b03bd8d908
zhangsheng377 <435878393@qq.com>
2018-09-09 19:35:35 +0800

    舵机用完即断电，使得用户可以手动开关灯~~使用红外接收模块接收米家网关发出的之前存储的信号

--- light/light.ino
+++ light/light.ino
@@ -9,11 +9,19 @@ const int button_read_pin = 3;
 
 #include <Servo.h>
 int servo_degree = 0;
+int servo_degree_old = 0;
 Servo myservo;  // create servo object to control a servo
 const int myservo_pin = 9;
+const int myservo_power_pin = 8;
 
 #include <EEPROM.h>
 const int eeAddress = 0;   //Location we want the data to be put.
+
+const int test_LED_power_pin = 5;
+const int test_LED_ground_pin = 6;
+
+const int IF_reciever_power_pin = 12;
+const int IF_reciever_sign_pin = 13;
 
 void setup() {
   // initialize serial:
@@ -28,12 +36,25 @@ void setup() {
   mySerial.println("Hello, world?");
 
   pinMode(button_HIGH_pin, OUTPUT);
-  pinMode(button_read_pin, INPUT_PULLUP);  // 上拉，普通状态为HIGH
   digitalWrite(button_HIGH_pin, LOW);
+  pinMode(button_read_pin, INPUT_PULLUP);  // 上拉，普通状态为HIGH
 
   myservo.attach(myservo_pin);  // attaches the servo on pin 9 to the servo object
-  
+
   EEPROM.get(eeAddress, servo_degree);
+  servo_degree_old = servo_degree;
+
+  pinMode(myservo_power_pin, OUTPUT);
+  digitalWrite(myservo_power_pin, LOW);
+
+  pinMode(test_LED_power_pin, OUTPUT);
+  digitalWrite(test_LED_power_pin, LOW);
+  pinMode(test_LED_ground_pin, OUTPUT);
+  digitalWrite(test_LED_ground_pin, LOW);
+
+  pinMode(IF_reciever_power_pin, OUTPUT);
+  digitalWrite(IF_reciever_power_pin, HIGH);
+  pinMode(IF_reciever_sign_pin, INPUT_PULLUP);
 }
 
 void loop() {
@@ -60,18 +81,30 @@ void loop() {
   } else if (20 > servo_degree) {
     servo_degree = 20;
   }
-  Serial.println(servo_degree);
-  myservo.write(servo_degree);                  // sets the servo position according to the scaled value
-  EEPROM.put(eeAddress, servo_degree);
+  if (servo_degree_old != servo_degree) {
+    digitalWrite(myservo_power_pin, HIGH);
+    Serial.println(servo_degree);
+    myservo.write(servo_degree);                  // sets the servo position according to the scaled value
+    EEPROM.put(eeAddress, servo_degree);
+    servo_degree_old = servo_degree;
+  }
   delay(80);
+  digitalWrite(myservo_power_pin, LOW);
+
+  if (LOW == digitalRead(IF_reciever_sign_pin)) {
+    digitalWrite(test_LED_power_pin, HIGH);
+    delay(1000);
+  } else {
+    digitalWrite(test_LED_power_pin, LOW);
+  }
 }
 
 /*
   SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
+  hardware serial RX.  This routine is run between each
+  time loop() runs, so using delay inside loop can delay
+  response.  Multiple bytes of data may be available.
+*/
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
@@ -86,5 +119,3 @@ void serialEvent() {
     }
   }
 }
-
-
